058bef24a4ce051f6e2dde30aa047e1414992e06
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2020-04-23 16:55:29 +0200

    revertato i prefab modificati solo nelle scene

---
+++ Assets/Bullet/Scripts/Behaviour/Attack/Attack_ReflectBullet_SkillBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_ReflectBullet_SkillBehaviour : BaseSkillBehaviour
+{
+    BulletBase otherBullet;
+    GenericEnemy genericEnemy;
+
+    protected override void OnEnterCollider(Collider other)
+    {
+        base.OnEnterCollider(other);
+
+        otherBullet = other.gameObject.GetComponentInParent<BulletBase>();
+
+        genericEnemy = other.gameObject.GetComponentInParent<GenericEnemy>();
+
+        if (otherBullet)
+        {
+
+        }
+    }
+}
