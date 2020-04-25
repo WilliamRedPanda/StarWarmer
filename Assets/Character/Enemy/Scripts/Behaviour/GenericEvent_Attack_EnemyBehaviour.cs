1696eaf92e3dbff09511e9dea08fdb1e55a73b40
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2020-04-25 15:22:23 +0200

    Creato GenericEvent_Attack_EnemyBehaviour per eventi generici modificabili da inspector

---
+++ Assets/Character/Enemy/Scripts/Behaviour/GenericEvent_Attack_EnemyBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GenericEvent_Attack_EnemyBehaviour : BaseEnemyBehaviour
+{
+    [SerializeField] UnityEvent OnStartAttack;
+    [SerializeField] UnityEvent OnEndAttack;
+
+    protected override void OnStartPhaseAttack()
+    {
+        base.OnStartPhaseAttack();
+        OnStartAttack.Invoke();
+    }
+
+    protected override void OnEndPhaseAttack()
+    {
+        base.OnEndPhaseAttack();
+        OnEndAttack.Invoke();
+    }
+}
