993478971e1d8e8e72141a8a005d5dbfac368820
WilliamLacalamita <william.lacalamita@gmail.com>
2020-05-15 16:19:17 +0200

    Shooter Porjectile Remake

---
+++ Assets/VFX/Enemies VFX/Shooter Projectile/Dissolve.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+    Renderer render;
+    private float dissolveProgress = 1;
+    public float dissolveVelocityMultiplier = 1;
+    void Start()
+    {
+        //render = GetComponent<Renderer>();
+        //render.material.SetFloat("_DissolveScale", 1f);
+        //dissolveProgress = 1;
+    }
+
+    private void OnEnable()
+    {
+        render = GetComponent<Renderer>();
+        render.material.SetFloat("_DissolveScale", 1f);
+        dissolveProgress = 1;
+    }
+
+    void Update()
+    {
+        if (render.material.GetFloat("_DissolveScale") > 0)
+        {
+            dissolveProgress -= Time.deltaTime * dissolveVelocityMultiplier;
+            render.material.SetFloat("_DissolveScale", dissolveProgress);
+        }
+    }
+}
