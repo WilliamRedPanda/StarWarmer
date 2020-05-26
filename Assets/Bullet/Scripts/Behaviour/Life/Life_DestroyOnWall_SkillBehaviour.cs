52f69e7772b906a8105e7be322c81aaf7e2021c8
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2020-05-26 19:40:04 +0200

    fix skill destroy on shoot

--- Assets/Bullet/Scripts/Behaviour/Life/Life_DestroyOnWall_SkillBehaviour.cs
+++ Assets/Bullet/Scripts/Behaviour/Life/Life_DestroyOnWall_SkillBehaviour.cs
@@ -4,12 +4,57 @@ using UnityEngine;
 
 public class Life_DestroyOnWall_SkillBehaviour : BaseSkillBehaviour
 {
-    protected override void OnEnterCollider(Collider other)
+    Collider[] colliders;
+    bool b;
+
+    //protected override void OnShoot()
+    //{
+    //    base.OnShoot();
+    //    StartCoroutine(PostCheckCollide());
+    //}
+
+    RaycastHit hit;
+
+    protected override void FixedTick()
     {
-        base.OnEnterCollider(other);
-        if (other.tag == "Wall")
+        base.FixedTick();
+        colliders = Physics.OverlapSphere(skill.transform.position, 0.1f);
+        for (int i = 0; i < colliders.Length; i++)
         {
-            skill.Return();
+            if (colliders[i].tag == "Wall")
+            {
+                skill.Return();
+            }
         }
+
+        //if (Physics.SphereCast(skill.transform.position, 0.1f, Vector3.zero, out hit, 0.11f))
+        //{
+        //    if (hit.transform.tag == "Wall")
+        //    {
+        //        skill.Return();
+        //    }
+        //}
     }
+
+    //IEnumerator PostCheckCollide()
+    //{
+    //    yield return new WaitForSeconds(0.1f);
+    //    b = true;
+    //}
+
+    //protected override void OnEnterCollider(Collider other)
+    //{
+    //    base.OnEnterCollider(other);
+    //    if (other.tag == "Wall" && b)
+    //    {
+    //        skill.Return();
+    //    }
+    //}
+
+    //protected override void OnReturn()
+    //{
+    //    base.OnReturn();
+    //    StopCoroutine(PostCheckCollide());
+    //    b = false;
+    //}
 }
