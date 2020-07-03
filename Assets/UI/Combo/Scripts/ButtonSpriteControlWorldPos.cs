fa2812cb7b436d97127a750c520dab02728eb922
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2020-07-03 17:52:43 +0200

    Cambio tasti a seconda del controller nel tutorial

---
+++ Assets/UI/Combo/Scripts/ButtonSpriteControlWorldPos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSpriteControlWorldPos : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] InputData inputData;
+    [SerializeField] CurrentControllerManager currentControllerManager;
+
+    private void OnEnable()
+    {
+        currentControllerManager.OnChangeController += SetSprite;
+    }
+
+    private void OnDisable()
+    {
+        currentControllerManager.OnChangeController -= SetSprite;
+    }
+
+    public void SetSprite(inputDevice inputDevice)
+    {
+        switch (inputDevice)
+        {
+            case inputDevice.keyboard:
+                spriteRenderer.sprite = inputData.keySprite;
+                break;
+            case inputDevice.playStation:
+                spriteRenderer.sprite = inputData.PSSprite;
+                break;
+            case inputDevice.xBox:
+                spriteRenderer.sprite = inputData.XboxSprite;
+                break;
+            default:
+                break;
+        }
+    }
+}
