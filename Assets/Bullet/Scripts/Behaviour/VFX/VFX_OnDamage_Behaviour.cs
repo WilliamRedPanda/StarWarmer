66c0641a3e6efa54f966458b7136eff689bcfc92
Matteo Mastropasqua <teo.mastropasqua@gmail.com>
2020-07-13 17:36:58 +0200

    Reposition vfx hit of the spear

--- Assets/Bullet/Scripts/Behaviour/VFX/VFX_OnDamage_Behaviour.cs
+++ Assets/Bullet/Scripts/Behaviour/VFX/VFX_OnDamage_Behaviour.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class VFX_OnDamage_Behaviour : BaseSkillBehaviour
 {
     [SerializeField] ParticleSystem vfxPrefab;
+    [SerializeField] Transform vfxPosition;
 
     ParticleSystem vfx;
 
     protected override void OnDamage(IDamageable _damageable)
     {
         base.OnShoot();
-        vfx = Instantiate(vfxPrefab.gameObject, skill.transform.position, Quaternion.LookRotation(skill.shooter.aimDirection)).GetComponent<ParticleSystem>();
+        vfx = Instantiate(vfxPrefab.gameObject, vfxPosition ? vfxPosition.position : skill.transform.position, Quaternion.LookRotation(skill.shooter.aimDirection)).GetComponent<ParticleSystem>();
         vfx.Play();
         StartCoroutine(Corutine());
     }
